83505235319d2d20801a80069d2391a758a8ae2d
DmitryPopov1988 <42721137+DmitryPopov1988@users.noreply.github.com>
2024-07-22 23:30:03 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,20 +1,14 @@
 import fs from 'fs/promises';
 import path from 'path';
-import { QUESTION_CONTENT } from './constants'; 
 
-const BASE_DIRECTORY = path.resolve(QUESTION_CONTENT.RELATIVE_URL);
+const isFileAvailible = async (dir: string, filename: string): Promise<boolean> => {
+try {
+const filePath: string = path.join(path.resolve(dir), filename);
+await fs.stat(filePath);
+return true;
+} catch (e) {
+return false;
+}
+}
 
-const isFileAvailable = async (dir: string, filename: string): Promise<boolean> => {
-
-  try {
-
-    const filePath = path.join(BASE_DIRECTORY, filename);
-    const files = await fs.readdir(BASE_DIRECTORY, { withFileTypes: true });
-    return files.some(file => file.isFile() && file.name === filename);
-  } catch (error) {
-    console.error('Error reading directory:', error);
-    return false;
-  }
-};
-
-export default { isFileAvailable };
+export default { isFileAvailible );
