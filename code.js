08559a24b4bcdd03b35848e1ffe8075238841e1c
DmitryPopov1988 <42721137+DmitryPopov1988@users.noreply.github.com>
2024-07-23 01:29:46 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,16 @@
 import fs from 'fs/promises';
-const ALLOWED_PATH = '../../shared';
+import { QUESTION_CONTENT } from '@core/services/constants';
 
-const isFileAvailable = async (dir: string, filename: string): Promise<boolean> => {
+const ALLOWED_PATH = QUESTION_CONTENT.RELATIVE_URL;
+
+const sanitize = (fileName: string) => {
+  if (!fileName.includes('..')) throw new Error('Invalid file name.');
+  return fileName;
+};
+
+const isFileAvailable = async (filename: string): Promise<boolean> => {
   try {
-    await fs.stat(ALLOWED_PATH + filename); // test 34
+    await fs.stat(ALLOWED_PATH + sanitize(filename));
     return true;
   } catch (e) {
     return false;
