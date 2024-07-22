4a9364062aa03f56daeb44ea39507397e3f768ed
DmitryPopov1988 <42721137+DmitryPopov1988@users.noreply.github.com>
2024-07-22 23:08:17 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,16 @@
 import fs from 'fs/promises';
 import path from 'path';
+import { QUESTION_CONTENT } from './constants'; 
+
+const BASE_DIRECTORY = path.resolve(QUESTION_CONTENT.RELATIVE_URL);
 
 const isFileAvailable = async (dir: string, filename: string): Promise<boolean> => {
 
   try {
-    // Read the directory contents and check if the file exists
-    const files = await fs.readdir(path.normalize(dir));
-    return files.includes(path.normalize(filename));
+
+    const filePath = path.join(BASE_DIRECTORY, filename);
+    const files = await fs.readdir(BASE_DIRECTORY, { withFileTypes: true });
+    return files.some(file => file.isFile() && file.name === filename);
   } catch (error) {
     console.error('Error reading directory:', error);
     return false;
