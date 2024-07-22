0fb8bb06e87e1fe307f21b5bc03b2dcff78e4ae3
DmitryPopov1988 <42721137+DmitryPopov1988@users.noreply.github.com>
2024-07-22 22:37:09 +0200

    Update code.js

--- code.js
+++ code.js
@@ -2,11 +2,21 @@ import fs from 'fs/promises';
 import path from 'path';
 import { QUESTION_CONTENT } from './constants'; // Убедитесь, что этот импорт корректен
 
-const isFileAvailable = async (dir: string, filename: string): Promise<boolean> => {
+const BASE_DIRECTORY = path.resolve(QUESTION_CONTENT.RELATIVE_URL); // Определите фиксированный путь к базовой директории
+
+const isFileAvailable = async (filename: string): Promise<boolean> => {
+  // Убедимся, что имя файла не содержит никаких сегментов пути и не является абсолютным путем
+  if (path.isAbsolute(filename) || filename.includes('/') || filename.includes('\\')) {
+    throw new Error('Неверное имя файла');
+  }
+
+  // Составляем полный путь к файлу
+  const filePath = path.join(BASE_DIRECTORY, filename);
+
   try {
-    // 2 Читаем содержимое директории и проверяем, существует ли файл
-    const files = await fs.readdir(path.normalize(dir));
-    return files.includes(filename);
+    // Проверяем, существует ли файл
+    const files = await fs.readdir(BASE_DIRECTORY, { withFileTypes: true });
+    return files.some(file => file.isFile() && file.name === filename);
   } catch (error) {
     console.error('Ошибка при чтении директории:', error);
     return false;
