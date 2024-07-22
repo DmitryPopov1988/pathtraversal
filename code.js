4cfb4afa25fea9f9d408730302278afbe93e1d4b
DmitryPopov1988 <42721137+DmitryPopov1988@users.noreply.github.com>
2024-07-22 23:49:16 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,28 +1,18 @@
 import fs from 'fs/promises';
 import path from 'path';
 
-const BASE_DIRECTORY = path.resolve('path/to/allowed/base/directory'); // Определите фиксированный путь к базовой директории
 
-const isFileAvailable = async (filename: string): Promise<boolean> => {
-  // Убедитесь, что имя файла не содержит никаких сегментов пути и не является абсолютным путем
-  if (path.isAbsolute(filename) || filename.includes('/') || filename.includes('\\')) {
-    throw new Error('Неверное имя файла');
-  }
+const isFileAvailable = async (dir: string, filename: string): Promise<boolean> => {
+
 
   // Составляем полный путь к файлу
-  const filePath: string = path.join(BASE_DIRECTORY, filename);
+  const filePath: string = path.join(dir, filename);
 
   try {
     // Проверяем, существует ли файл
     await fs.stat(filePath);
     return true;
   } catch (e) {
-    // Если файл не существует, fs.stat выбросит ошибку
-    if (e.code === 'ENOENT') {
-      return false;
-    }
-    // Обработка других ошибок
-    console.error('Ошибка при проверке файла:', e);
     return false;
   }
 };
