8647d7f6a7ce40f82595aac84bcdf4932548c009
DmitryPopov1988 <42721137+DmitryPopov1988@users.noreply.github.com>
2024-07-23 01:30:10 +0200

    Update code.js

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ import { QUESTION_CONTENT } from '@core/services/constants';
 const ALLOWED_PATH = QUESTION_CONTENT.RELATIVE_URL;
 
 const sanitize = (fileName: string) => {
-  if (!fileName.includes('..')) throw new Error('Invalid file name.');
+  if (fileName.includes('..')) throw new Error('Invalid file name.');
   return fileName;
 };
 
