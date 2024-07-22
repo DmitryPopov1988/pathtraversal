cd8b92ec3810f4f3220578ba862554acfbb1356a
DmitryPopov1988 <42721137+DmitryPopov1988@users.noreply.github.com>
2024-07-23 01:12:20 +0200

    Update code.js

--- code.js
+++ code.js
@@ -1,29 +1,9 @@
 import fs from 'fs/promises';
-import path from 'path';
-
+const ALLOWED_PATH = '../../shared';
 
 const isFileAvailable = async (dir: string, filename: string): Promise<boolean> => {
-
-  if (!dir.startWith('../../shared') {
-  throw new Error('Invalid path specified.');
-  }
-
-if (
-    filename.includes('..') ||
-    filename.includes('/') ||
-    filename.includes('\\')
-  ) {
-    throw new Error('Invalid file name.');
-  }
-
-
-
-
-
-
   try {
-    const filePath = path.join(dir, filename);
-    await fs.stat(filePath); // test 33
+    await fs.stat(ALLOWED_PATH + filename); // test 34
     return true;
   } catch (e) {
     return false;
